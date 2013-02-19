ab5fca6e19d8c6588a699e700be0877cc13eb38a
Marek Marczykowski <marmarek@invisiblethingslab.com>
2013-02-19 03:33:01 +0100

    Dot allow to run command in paused VM

--- qubesmanager/main.py
+++ qubesmanager/main.py
@@ -1124,7 +1124,7 @@ class VmManagerWindow(Ui_VmManagerWindow, QMainWindow):
             self.action_updatevm.setEnabled(vm.is_updateable() or vm.qid == 0)
             self.action_toggle_audio_input.setEnabled(vm.last_running and vm.qid != 0 and \
                     session_bus.name_has_owner('org.QubesOS.Audio.%s' % vm.name))
-            self.action_run_command_in_vm.setEnabled(vm.qid != 0)
+            self.action_run_command_in_vm.setEnabled(not vm.last_power_state == "Paused" and vm.qid != 0)
             self.action_set_keyboard_layout.setEnabled(vm.qid != 0 and vm.last_running)
         else:
             self.action_settings.setEnabled(False)
