712ff1fc1c9bf491cb9218faa116e5c0d9bfd42b
Marta Marczykowska-Górecka <marmarta@invisiblethingslab.com>
2025-01-03 17:43:40 +0100

    Remove ui files from coverage report

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -m coverage run --omit="test-packages/*,qubesmanager/tests/*,*/core-admin-client/*" -m pytest -vv qubesmanager/tests
+python3 -m coverage run --omit="test-packages/*,qubesmanager/tests/*,*/core-admin-client/*,qubesmanager/ui*" -m pytest -vv qubesmanager/tests
